3dfdd8112f91f95778172eef76f25d6870bf83c4
abubakirilhomov <2ilhomovabubakir2@gmail.com>
2024-12-03 12:38:57 +0500

    fds

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Home from './home'
+
+const MainLayout = () => {
+  return (
+    <View>
+      <Home />
+    </View>
+  )
+}
+
+export default MainLayout
---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Link } from 'expo-router'
+
+const App = () => {
+    return (
+        <View>
+            <Link href="/home" className='text-red-500'>Go to home</Link>
+        </View>
+    )
+}
+
+export default App
